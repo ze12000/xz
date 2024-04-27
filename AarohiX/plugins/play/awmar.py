6e172aba54870e03d980ad3c0c0d4c2754bde27c
ze12000 <167222274+ze12000@users.noreply.github.com>
2024-04-27 22:48:09 +0300

    Update awmar.py

--- AarohiX/plugins/play/awmar.py
+++ AarohiX/plugins/play/awmar.py
@@ -1,16 +1,12 @@
 import asyncio
 import config
 from pyrogram import Client, filters
-from pyrogram import filters
-from strings import get_command
-from strings.filters import command
 from AarohiX import app
-from config.config import OWNER_ID
+from config import OWNER_ID
 from AarohiX.misc import SUDOERS
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
 from pyrogram.types import (InlineKeyboardButton,CallbackQuery,InlineKeyboardMarkup, Message)
 from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AarohiX.misc import SUDOERS
 
 @app.on_message(command(["الاوامر","اوامر البوت"]))
 async def abrag(c: Client, m: Message):
